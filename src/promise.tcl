77ebbf216af6026a57746af66ed9a7647f5b693d
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2016-01-14 20:22:28 +0530

    Bunch of documentation comments

--- src/promise.tcl
+++ src/promise.tcl
@@ -53,10 +53,18 @@ oo::class create promise::Promise {
     variable _nrefs
     
     constructor {cmd} {
+        # Create a promise for the asynchronous operation to be initiated
+        # by $cmd.
+        # cmd - a command prefix that should initiate an asynchronous
+        #  operation.
+        # The command prefix $cmd is passed an additional argument - the
+        # name of this Promise object. It should arrange for one of the
+        # object's settle methods [resolve], [resolve_with_promise] or
+        # [reject] to be called when the operation completes.
+        
         set _state PENDING
         set _handlers [list ]
         set _nrefs 0
-        set _at_least_one_handler_run 0
         
         # Errors in the construction command are returned via
         # the standard mechanism of reject.
@@ -68,12 +76,29 @@ oo::class create promise::Promise {
     }
 
     destructor {
+        # Destroys the object.
+        #
+        # This method should not be generally called directly as [Promise]
+        # objects are garbage collected either automatically or via the [ref]
+        # and [unref] methods.
     }
     
-    method state {} { return $_state }
+    method state {} {
+        # Returns the current state of the promise
+        #
+        # The promise state may be one of the values 'PENDING',
+        # 'FULFILLED', 'REJECTED' or 'ATTACHED'
+        return $_state
+    }
     
     method value {} {
-        # Note even if _value is set, it may be not the "final" value
+        # Returns the settled value for the promise
+        #
+        # The returned value may be the fulfilled value or the rejected
+        # value depending on whether the associated operation was successfully
+        # completed or failed.
+        #
+        # An error is raised if the promise is not settled yet.
         if {$_state ni {FULFILLED REJECTED}} {
             error "Value is not set."
         }
@@ -81,10 +106,14 @@ oo::class create promise::Promise {
     }
 
     method ref {} {
+        # Increments the reference count for the object
         incr _nrefs
     }
 
     method unref {} {
+        # Decrements the reference count for the object
+        #
+        # The object may have been destroyed when the call returns.
         incr _nrefs -1
         my GC
     }
@@ -162,7 +191,6 @@ oo::class create promise::Promise {
             return
         }
 
-        set _at_least_one_handler_run 1; # Needed for garbage collection
         if {$_state eq "FULFILLED"} {
             set ix 0
         } else {
@@ -274,6 +302,16 @@ proc promise::then_reject {errval} {
 }
 
 proc promise::all {promises} {
+    # Returns a promise that resolves or rejects when all promises
+    # in the $promises argument have resolved or any one has rejected
+    # promises - a list of Promise objects
+    #
+    # If any of $promises rejects, then the promise returned by the
+    # command will reject with the same value. Otherwise, the promise
+    # will resolve when all promises have resolved.
+    # The resolved value will be a list of the resolved
+    # values of the contained promises.
+    
     set all_promise [Promise new [lambda {promises prom} {
         if {[llength $promises] == 0} {
             $prom resolve {}
@@ -289,7 +327,13 @@ proc promise::all {promises} {
     return $all_promise
 }
 
-proc promise::alln args {
+proc promise::all* args {
+    # Returns a promise that resolves or rejects when all promises
+    # in the $args argument have resolved or any one has rejected
+    # args - list of Promise objects
+    # This command is identical to the all command except that it takes
+    # multiple arguments, each of which is a Promise object. See [all]
+    # for a description.
     return [all $args]
 }
 
@@ -302,6 +346,9 @@ proc promise::alln args {
 #  value - the value of the currently resolved promise or error description
 #   in case rejected
 proc promise::_all_helper {all_promise remaining_promises values resolution value} {
+    # TBD - this does not seem the best way to do this. In particular,
+    # an earlier promise might stay pending though a later promise has
+    # rejected causing us to wait unnecesarily
     if {![info object isa object $all_promise]} {
         # The object has been deleted. Naught to do
         return
@@ -332,6 +379,11 @@ proc promise::_all_helper {all_promise remaining_promises values resolution valu
 }
 
 proc promise::race {promises} {
+    # Returns a promise that resolves or rejects when any promise
+    # in the $promises argument is resolved or rejected
+    #   promises - a list of Promise objects
+    # The returned promise will resolve and reject with the same value
+    # as the first promise in $promises that resolves or rejects.
     set race_promise [Promise new [lambda {promises prom} {
         if {[llength $promises] == 0} {
             $prom reject [_make_errval PROMISE RACE EMPTYSET "Promise set is empty"]
@@ -343,6 +395,16 @@ proc promise::race {promises} {
     } $promises]]
 
     return $race_promise
+}
+
+proc promise::race* {args} {
+    # Returns a promise that resolves or rejects when any promise
+    # in the passed arguments is resolved or rejected
+    #   args - list of Promise objects
+    # This command is identical to the all command except that it takes
+    # multiple arguments, each of which is a Promise object. See [race]
+    # for a description.
+    return [race $args]
 }
 
 proc promise::_make_errval {args} {
@@ -391,11 +453,11 @@ proc promise::pconnect {args} {
     } $args]]
 }
 
-proc promise::ReadChannel {prom chan data} {
+proc promise::_read_channel {prom chan data} {
     set newdata [read $chan]
     if {[string length $newdata] || ![eof $chan]} {
         append data $newdata
-        fileevent $chan readable [list [namespace current]::ReadChannel $prom $chan $data]
+        fileevent $chan readable [list [namespace current]::_read_channel $prom $chan $data]
         return
     }
 
@@ -408,9 +470,9 @@ proc promise::ReadChannel {prom chan data} {
         close $chan
     } result edict]
     if {$code} {
-        Settle $prom $code $result $edict
+        _settle $prom $code $result $edict
     } else {
-        Settle $prom 0 $data
+        _settle $prom 0 $data
     }
 }
 
@@ -418,11 +480,11 @@ proc promise::pexec {args} {
     return [Promise new [lambda {open_args prom} {
         set chan [open |$open_args r]
         fconfigure $chan -blocking 0
-        fileevent $chan readable [list promise::ReadChannel $prom $chan ""]
+        fileevent $chan readable [list promise::_read_channel $prom $chan ""]
     } $args]]
 }        
 
-proc promise::Settle {prom code result {edict {}}} {
+proc promise::_settle {prom code result {edict {}}} {
     if {![info object isa object $prom]} {
         # The object has been deleted. Naught to do
         return
@@ -437,11 +499,25 @@ proc promise::Settle {prom code result {edict {}}} {
 }
         
 proc promise::ptask {script} {
+    # Creates a new Tcl thread to run the specified script and returns
+    # a promise for it
+    #   script - script to run in the thread
+    # Returns a promise that will be settled by the result of the script
+    #
+    # The `ptask` command runs the specified script in a new Tcl
+    # thread. The promise returned from this command will be resolved
+    # with the result of the script if it completes
+    # successfully. Otherwise, the promise will be rejected with an
+    # error value that is a pair containing the error message and
+    # error dictionary from the script failure.
+    #
+    # The command requires the Thread package to be loaded.
+
     uplevel #0 package require Thread
     proc [namespace current]::ptask script { 
         return [Promise new [lambda {script prom} {
             set thread_script [string map [list %PROM% $prom %TID% [thread::id] %SCRIPT% $script] {
-                thread::send -async %TID% [list ::promise::Settle %PROM% [catch {%SCRIPT%} result edict] $result $edict]
+                thread::send -async %TID% [list ::promise::_settle %PROM% [catch {%SCRIPT%} result edict] $result $edict]
             }]
             thread::create $thread_script
         } $script]]
@@ -450,14 +526,108 @@ proc promise::ptask {script} {
 }
 
 proc promise::pworker {tpool script} {
+    # Runs a script in a worker thread from a thread pool and returns a promise for the same
+    #   tpool - thread pool identifier
+    #   script - script to run in the worker thread
+    # Returns a promise that will be settled by the result of the script
+    #
+    # The Thread package allows creation of a thread pool
+    # with the 'tpool create' command. The `pworker` command runs the
+    # specified script in a worker thread from a thread pool. The promise
+    # returned from this command will be resolved with the
+    # result of the script if it completes successfully. Otherwise,
+    # the promise will be rejected with an error value that is a pair
+    # containing the error message and error dictionary from the script
+    # failure.
+
+    
     # No need for package require Thread since if tpool is passed to
     # us, Thread must already be loaded
     return [Promise new [lambda {tpool script prom} {
         set thread_script [string map [list %PROM% $prom %TID% [thread::id] %SCRIPT% $script] {
-            thread::send -async %TID% [list ::promise::Settle %PROM% [catch {%SCRIPT%} result edict] $result $edict]
+            thread::send -async %TID% [list ::promise::_settle %PROM% [catch {%SCRIPT%} result edict] $result $edict]
         }]
         tpool::post -detached -nowait $tpool $thread_script
     } $tpool $script]]
+}
+
+proc promise::_document_self {path args} {
+    # Generates documentation for the package in HTML format.
+    # path - path to the output file
+
+    package require ruff
+
+    set intro {
+        This package implements the *promise* abstraction for
+        asynchronous programming. This document is the reference for
+        commands and classes implemented by the package. For a
+        tutorial introduction to promises, usage guide and examples, see
+        http://www.magicsplat.com/blog/promises.
+    }
+
+    set abstraction {
+        The promise abstraction encapsulates the eventual result of a
+        possibly asynchronous operation.
+
+        This package follows the terminology used in the Javascript world,
+        in particular the ECMAScript 2015 Language specification though
+        details of implementation differ.
+        
+        From an application's perspective, a Promise object may be in one
+        of three states:
+
+        - FULFILLED
+        - REJECTED
+        - PENDING, if it is neither of the above
+
+        Though the above specification does not explicitly assign meanings
+        to these states, in practice FULFILLED and REJECTED are associated
+        with successful and failed completion of the operation respectively
+        while PENDING reflects the operation has not completed.
+
+        Some additional terms:
+
+        A promise is said to be settled if it is either in the FULFILLED
+        or REJECTED state. A promise that is settled will thereafter never
+        change state.
+
+        A promise is said to be resolved if it is settled or if it
+        is attached to the state of another promise.
+
+        In this package, the Promise class implements promises.
+    }
+    
+    set gc {
+        TclOO objects are not garbage collected and have to be explicitly
+        destroyed. In the case of promises, because of their asynchronous
+        nature, it is often not clear to applications when the promise
+        objects should be destroyed.
+
+        Therefore the package internally manages the lifetime of Promise
+        objects such that they are automatically destroyed once they are
+        settled and at least one fulfillment or rejection handler has been
+        run. This removes the burden from the application in the most common
+        usage scenarios. In cases where the application wants the object to
+        persist, for example, when the resolved value is accessed multiple
+        times, it can use the 'ref' and 'unref' methods of the
+        [::promise::Promise] object to explicitly manage the lifetime of the
+        object.
+        
+    }
+    
+    ::ruff::document_namespaces html [namespace current] \
+        -autolink false \
+        -recurse true \
+        -output $path \
+        -titledesc "promise (V[version])" \
+        -copyright "[clock format [clock seconds] -format %Y] Ashok P. Nadkarni" \
+        {*}$args \
+        -preamble [dict create :: \
+                       [list \
+                            Introduction [::ruff::extract_docstring $intro] \
+                            {The promise abstraction} [::ruff::extract_docstring $abstraction] \
+                            {Garbage collection} [::ruff::extract_docstring $gc] \
+                           ]]
 }
 
 if {0} {
@@ -499,6 +669,9 @@ if {[info exists ::argv0] &&
             } else {
                 error "Cannot create distribution from a .tm file"
             }
+        }
+        doc* {
+            promise::_document_self promise-[promise::version].html
         }
         default {
             error "Unknown option/command \"[lindex $::argv 0]\""
